13ed4008c31159328108bc91341351e733d7267e
kimhyang <gim-hyang-gi@gimhyang-gis-Mac.local>
2015-06-01 21:26:29 +0900

    commit

--- DWFMS/AppDelegate.m
+++ DWFMS/AppDelegate.m
@@ -88,7 +88,11 @@ didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
     NSLog(@"Failed to get token, error: %@", error);
 }
-
+- (void)application:(UIApplication *)application
+didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    NSLog(@" recive aspn %@ ",userInfo);
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
